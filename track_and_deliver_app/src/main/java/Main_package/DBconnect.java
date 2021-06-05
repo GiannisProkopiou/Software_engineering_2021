928e95c9043ae208c6378f9bb1f74fb8049d77f7
sina_giannis <49285637+sinakhs@users.noreply.github.com>
2021-06-05 20:50:33 +0300

    Created sign in system

--- track_and_deliver_app/src/main/java/Main_package/DBconnect.java
+++ track_and_deliver_app/src/main/java/Main_package/DBconnect.java
@@ -1,9 +1,12 @@
+package Main_package;
+
 import java.sql.*;
 import javax.swing.JOptionPane;
 public class DBconnect {
     public Connection conn=null;
     public static Connection GetConnection(){
         try{
+           
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/track_and_deliver_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","1999");
             //JOptionPane.showMessageDialog(null, "Connected to database");
             return conn;
